87767dc2e2821b3b67af6633812e8e1efb059981
Fabian Faessler <fabi@fabif.de>
2013-09-28 16:50:13 +0200

    pep8 stuff

--- nsupdate/main/views.py
+++ nsupdate/main/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.conf import settings
 import json
 
+
 class HomeView(TemplateView):
     template_name = "base.html"
 
@@ -11,6 +12,7 @@ class HomeView(TemplateView):
         context = super(HomeView, self).get_context_data(*args, **kwargs)
         context['nav_home'] = True
         return context
+
 
 class OverviewView(TemplateView):
     template_name = "main/overview.html"
@@ -24,4 +26,6 @@ class OverviewView(TemplateView):
 
 
 def MyIpView(request):
-    return HttpResponse(json.dumps({'ip':request.META['REMOTE_ADDR']}), content_type="application/json")
+    return HttpResponse(
+        json.dumps({'ip': request.META['REMOTE_ADDR']}),
+        content_type="application/json")
