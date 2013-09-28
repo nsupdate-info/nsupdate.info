dd614d193670b1c82e95cdd968965bfebfdc9a98
Fabian Faessler <fabi@fabif.de>
2013-09-28 23:45:46 +0200

    changed the host edit into a classbased view. added host delete

--- nsupdate/main/urls.py
+++ nsupdate/main/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from main.views import (
-    HomeView, OverviewView, HostView,
+    HomeView, OverviewView, HostView, DeleteHostView,
 )
 from api.views import (
     MyIpView, UpdateIpView, NicUpdateView
@@ -9,7 +9,8 @@ from api.views import (
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name="home"),
     url(r'^overview/$', OverviewView.as_view(), name='overview'),
-    url(r'^host/(?P<pk>\d+)/$', HostView, name='host_view'),
+    url(r'^host/(?P<pk>\d+)/$', HostView.as_view(), name='host'),
+    url(r'^delete_host/(?P<pk>\d+)/$', DeleteHostView.as_view(), name='delete_host'),
     url(r'^myip$', MyIpView),
     url(r'^updateip$', UpdateIpView),
     url(r'^nic/update$', NicUpdateView),
--- nsupdate/main/views.py
+++ nsupdate/main/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import TemplateView, CreateView
 from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView, DeleteView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
 from django.shortcuts import render, get_object_or_404
@@ -55,19 +56,45 @@ class OverviewView(CreateView):
         context['hosts'] = Host.objects.filter(created_by=self.request.user)
         return context
 
+class HostView(UpdateView):
+    model = Host
+    template_name = "main/host.html"
+    form_class = HostForm
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(HostView, self).dispatch(*args, **kwargs)
+    
+    def get_success_url(self):
+        return reverse('overview')
 
-@login_required
-def HostView(request,pk=None):
-    context = create_context(request)
-    context['nav_overview'] = True
-    context['HostForm'] = HostForm(request.user,instance=get_object_or_404(Host, pk=pk, created_by=request.user))
-    if request.method == "POST":
-        print "POST"
-        form = HostForm(request.user, request.POST,)
-        print form
-        if form.is_valid():
-            print "valid"
-            host = form.save(request.user)
-        context['HostForm'] = form
-    return render(request, "main/host.html", context)
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.created_by = self.request.user
+        self.object.save()
+        messages.add_message(self.request, messages.SUCCESS, 'Host updated.')
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HostView, self).get_context_data(*args, **kwargs)
+        context['nav_overview'] = True
+        context['hosts'] = Host.objects.filter(created_by=self.request.user)
+        return context
+
+class DeleteHostView(DeleteView):
+    model = Host
+    template_name = "main/delete_host.html"
+    form_class = HostForm
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(DeleteHostView, self).dispatch(*args, **kwargs)
+    
+    def get_success_url(self):
+        return reverse('overview')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(DeleteHostView, self).get_context_data(*args, **kwargs)
+        context['nav_overview'] = True
+        context['hosts'] = Host.objects.filter(created_by=self.request.user)
+        return context
+
 
