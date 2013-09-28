57e4ae3c26df0fa86d077b8e8b76bc60cf9b1254
Arne Schauf <arne@as-webservices.de>
2013-09-29 01:08:52 +0200

    minor fixes

--- nsupdate/main/urls.py
+++ nsupdate/main/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from main.views import (
-    HomeView, OverviewView, HostView, DeleteHostView,
+    HomeView, OverviewView, HostView, HostDeleteView,
 )
 from api.views import (
     MyIpView, UpdateIpView, NicUpdateView
@@ -9,8 +9,8 @@ from api.views import (
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name="home"),
     url(r'^overview/$', OverviewView.as_view(), name='overview'),
-    url(r'^host/(?P<pk>\d+)/$', HostView.as_view(), name='host'),
-    url(r'^delete_host/(?P<pk>\d+)/$', DeleteHostView.as_view(), name='delete_host'),
+    url(r'^host/(?P<pk>\d+)/$', HostView.as_view(), name='host_view'),
+    url(r'^host/(?P<pk>\d+)/delete/$', HostDeleteView.as_view(), name='host_delete'),
     url(r'^myip$', MyIpView),
     url(r'^updateip$', UpdateIpView),
     url(r'^nic/update$', NicUpdateView),
