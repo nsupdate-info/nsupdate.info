a4a183f7022d4a69fc896467a8ff367f539acdc9
Fabian Faessler <fabi@fabif.de>
2013-09-28 13:04:21 +0200

    added URL and simple view to get the user IP in a json format {'ip': '127.0.0.1'}

--- nsupdate/main/views.py
+++ nsupdate/main/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-
 from django.views.generic import TemplateView
+from django.http import HttpResponse
+import json
 
 class HomeView(TemplateView):
     template_name = "base.html"
@@ -9,3 +10,6 @@ class HomeView(TemplateView):
         context = super(HomeView, self).get_context_data(*args, **kwargs)
         context['nav_home'] = True
         return context
+
+def MyIpView(request):
+	return HttpResponse(json.dumps({'ip':request.META['REMOTE_ADDR']}), content_type="application/json")
