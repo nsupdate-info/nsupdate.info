22eecac4d5fad6a89deb6b1e966be47749cd4a87
Arne Schauf <arne@as-webservices.de>
2013-09-28 18:02:13 +0200

    Host model

--- nsupdate/main/models.py
+++ nsupdate/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Host(models.Model):
+    fqdn = models.CharField(max_length=256)
+    update_secret = models.CharField(max_length=256)
+
+    last_update = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return u"%s (%s)" % (self.fqdn, self.created_by)
+
