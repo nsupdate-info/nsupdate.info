4a3751732c032f4e4406f5a45f91fce9d73610c4
Arne Schauf <arne@as-webservices.de>
2013-09-28 18:33:45 +0200

    user profile edit name form

--- nsupdate/accounts/views.py
+++ nsupdate/accounts/views.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, UpdateView
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+from .forms import UserProfileForm
 
 
-class UserProfileView(TemplateView):
+class UserProfileView(UpdateView):
     template_name = "accounts/user_profile.html"
+    model = User
+    fields = ['first_name', 'last_name']
+    form_class = UserProfileForm
+
+    def get_object(self):
+        return self.request.user
+
+    def get_success_url(self):
+        return reverse('account_profile')
 
     def get_context_data(self, *args, **kwargs):
         context = super(UserProfileView, self).get_context_data(*args, **kwargs)
