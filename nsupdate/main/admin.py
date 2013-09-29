b60bd755094d56aae843af88114e89943e9c05f2
Arne Schauf <arne@as-webservices.de>
2013-09-29 14:10:14 +0200

    added domain blacklisting

--- nsupdate/main/admin.py
+++ nsupdate/main/admin.py
@@ -1,5 +1,6 @@
-from main.models import Host, Domain
+from main.models import Host, Domain, BlacklistedDomain
 from django.contrib import admin
 
+admin.site.register(BlacklistedDomain)
 admin.site.register(Domain)
 admin.site.register(Host)
