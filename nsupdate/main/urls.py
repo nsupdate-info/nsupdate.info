d8f42c1fb11643d381e3748803ee7420b58cc043
Fabian Faessler <fabi@fabif.de>
2013-09-29 16:58:02 +0200

    added help and about view

--- nsupdate/main/urls.py
+++ nsupdate/main/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, url
-from main.views import HomeView, OverviewView, HostView, DeleteHostView
+from main.views import HomeView, OverviewView, HostView, DeleteHostView, AboutView, HelpView
 from api.views import MyIpView, DetectIpView, NicUpdateView, AuthorizedNicUpdateView
 
 
 urlpatterns = patterns(
     '',
     url(r'^$', HomeView.as_view(), name="home"),
+    url(r'^about/$', AboutView.as_view(), name="about"),
+    url(r'^help/$', HelpView.as_view(), name="help"),
     url(r'^overview/$', OverviewView.as_view(), name='overview'),
     url(r'^host/(?P<pk>\d+)/$', HostView.as_view(), name='host_view'),
     url(r'^host/(?P<pk>\d+)/delete/$', DeleteHostView.as_view(), name='delete_host'),
