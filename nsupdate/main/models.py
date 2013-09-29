d44c8a7e90ef727f13c54e93de6d66a328ef9f2e
Arne Schauf <arne@as-webservices.de>
2013-09-29 14:08:22 +0200

    made home template and added admin link to nav

--- nsupdate/main/models.py
+++ nsupdate/main/models.py
@@ -1,7 +1,27 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
 from django.forms import ModelForm
+
+import re
+
+
+class BlacklistedDomain(models.Model):
+    domain = models.CharField(max_length=256, unique=True, help_text='Blacklisted domain. Evaluated as regex (search).')
+
+    last_update = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return u"%s" % (self.domain)
+
+
+def domain_blacklist_validator(value):
+    for bd in BlacklistedDomain.objects.all():
+        if re.search(bd.domain, value):
+            raise ValidationError(u'This domain is not allowed')
 
 
 class Domain(models.Model):
