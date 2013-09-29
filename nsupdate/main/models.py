844f08137c0663a96739f6b68ed835cff9724779
Fabian Faessler <fabi@fabif.de>
2013-09-29 14:36:29 +0200

    merge conflict again in models.py

--- nsupdate/main/models.py
+++ nsupdate/main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 from django.forms import ModelForm
