7f71bf756599843ac33b20b56a7c013bf9d5b940
Arne Schauf <arne@as-webservices.de>
2013-09-29 03:13:04 +0200

    piwik tracking

--- nsupdate/nsupdate/context_processors.py
+++ nsupdate/nsupdate/context_processors.py
@@ -6,4 +6,8 @@ def add_settings(request):
     context = {}
     context['WWW_IPV4_HOST'] = settings.WWW_IPV4_HOST
     context['WWW_IPV6_HOST'] = settings.WWW_IPV6_HOST
+    try:
+        context['ENABLE_PIWIK_PRODUCTION'] = settings.ENABLE_PIWIK_PRODUCTION
+    except:
+        pass
     return context
