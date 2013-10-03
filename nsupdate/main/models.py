42a89b4b42ee824696f43b302392b5df2272f963
Thomas Waldmann <tw@waldmann-edv.de>
2013-10-03 03:22:09 +0200

    fix pep8, fix indentation

--- nsupdate/main/models.py
+++ nsupdate/main/models.py
@@ -34,7 +34,8 @@ def domain_blacklist_validator(value):
 
 class Domain(models.Model):
     domain = models.CharField(max_length=256, unique=True)
-    nameserver_ip = models.IPAddressField(max_length=256,
+    nameserver_ip = models.IPAddressField(
+        max_length=256,
         help_text="An IP where the nsupdates for this domain will be sent to")
     nameserver_update_key = models.CharField(max_length=256)
 
