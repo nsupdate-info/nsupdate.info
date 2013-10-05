fdd979d5e1051cd94e9b96e1e591b5800f9f2e63
Thomas Waldmann <tw@waldmann-edv.de>
2013-10-05 04:17:32 +0200

    remove hardcoded PIWIK stuff

--- nsupdate/nsupdate/context_processors.py
+++ nsupdate/nsupdate/context_processors.py
@@ -6,8 +6,4 @@ def add_settings(request):
     context = {}
     context['WWW_IPV4_HOST'] = settings.WWW_IPV4_HOST
     context['WWW_IPV6_HOST'] = settings.WWW_IPV6_HOST
-    try:
-        context['ENABLE_PIWIK_PRODUCTION'] = settings.ENABLE_PIWIK_PRODUCTION
-    except AttributeError:
-        pass
     return context
