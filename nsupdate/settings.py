9dcefda8d346c5cb4145ff7f5110086ff131f010
Thomas Waldmann <tw@waldmann-edv.de>
2013-11-03 22:57:37 +0100

    make home view customizable via includes

--- nsupdate/settings.py
+++ nsupdate/settings.py
@@ -139,6 +139,7 @@ ROOT_URLCONF = 'nsupdate.urls'
 WSGI_APPLICATION = 'nsupdate.wsgi.application'
 
 TEMPLATE_DIRS = (
+    # you can add site-specific template dirs here to customize nsupdate.info
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
