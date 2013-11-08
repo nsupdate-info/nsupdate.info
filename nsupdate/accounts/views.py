b3ef4b0ce71206762463e7f54efdd3de1690ba35
Thomas Waldmann <tw@waldmann-edv.de>
2013-11-08 06:57:19 +0100

    removed unused PasswordChangeView class

--- nsupdate/accounts/views.py
+++ nsupdate/accounts/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.views.generic import TemplateView, UpdateView
+from django.views.generic import UpdateView
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
@@ -21,13 +21,4 @@ class UserProfileView(UpdateView):
     def get_context_data(self, *args, **kwargs):
         context = super(UserProfileView, self).get_context_data(*args, **kwargs)
         context['nav_user_profile'] = True
-        return context
-
-
-class PasswordChangeView(TemplateView):
-    template_name = "registration/password_change.html"
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(PasswordChangeView, self).get_context_data(*args, **kwargs)
-        context['nav_change_password'] = True
         return context
