93db75549810133d387833a3d4d5f70e0c4fb379
Thomas Waldmann <tw@waldmann-edv.de>
2013-11-29 22:24:52 +0100

    activate middleware for SocialAuth exception handling

--- nsupdate/settings.py
+++ nsupdate/settings.py
@@ -127,6 +127,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'social.apps.django_app.middleware.SocialAuthExceptionMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
