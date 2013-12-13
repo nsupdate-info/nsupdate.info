cc4cd1c805c4517ca3f25f1f28d63ef24c9904a2
Thomas Waldmann <tw@waldmann-edv.de>
2013-12-13 02:40:39 +0100

    travis: build on django 1.5 and 1.6

--- setup.py
+++ setup.py
@@ -42,8 +42,8 @@ setup(
     zip_safe=False,
     platforms='any',
     install_requires=[
-        'django >=1.5.3, <1.6',  # 1.5.3 has the session serializer configurable
-                                 # 1.6 is not tested yet
+        'django >=1.5.3, <1.7',  # 1.5.3 has the session serializer configurable
+                                 # 1.7 is not tested yet
         'dnspython',
         'south',
         'django-bootstrap-form',
