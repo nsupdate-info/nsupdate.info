e18b725096fd206fa4a02a083a0f7ab9ed1fa8e1
Thomas Waldmann <tw@waldmann-edv.de>
2013-12-15 17:16:04 +0100

    fix default settings module name at some places
    note: in production, you will usually have to specify an OWN module (like local_settings) that imports from nsupdate.settings.prod and then overrides whatever you need to be different

--- manage.py
+++ manage.py
@@ -4,6 +4,6 @@ import sys
 
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nsupdate.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nsupdate.settings.dev")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
