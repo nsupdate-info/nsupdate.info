f4be4ba6d0282870d3e3b022b963e23fa07385a7
Thomas Waldmann <tw@waldmann-edv.de>
2014-05-25 18:26:56 +0200

    add client_faults counter to admin display

--- nsupdate/main/admin.py
+++ nsupdate/main/admin.py
@@ -13,8 +13,8 @@ class DomainAdmin(admin.ModelAdmin):
 
 
 class HostAdmin(admin.ModelAdmin):
-    list_display = ("subdomain", "domain", "abuse", "abuse_blocked")
-    list_filter = ("domain", "created_by", "abuse", "abuse_blocked")
+    list_display = ("subdomain", "domain", "client_faults", "abuse", "abuse_blocked")
+    list_filter = ("domain", "created_by", "client_faults", "abuse", "abuse_blocked")
 
 
 class BlacklistedDomainAdmin(admin.ModelAdmin):
