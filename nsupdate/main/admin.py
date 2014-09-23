c15bfad34e1f81d62d7855097321a8368220f584
Thomas Waldmann <tw@waldmann-edv.de>
2014-09-23 23:11:55 +0200

    add search_fields to Host and Domain admin

--- nsupdate/main/admin.py
+++ nsupdate/main/admin.py
@@ -10,11 +10,13 @@ from .models import Host, Domain, BlacklistedDomain, ServiceUpdater, ServiceUpda
 class DomainAdmin(admin.ModelAdmin):
     list_display = ("domain", "public", "available", "created_by")
     list_filter = ("created", "public", "available")
+    search_fields = ("domain", "created_by__username", "created_by__email")
 
 
 class HostAdmin(admin.ModelAdmin):
     list_display = ("subdomain", "domain", "created_by", "client_faults", "abuse", "abuse_blocked")
     list_filter = ("created", "abuse", "abuse_blocked", "domain")
+    search_fields = ("subdomain", "created_by__username", "created_by__email")
 
 
 class BlacklistedDomainAdmin(admin.ModelAdmin):
