8624d5dbd9d0cc687154983511c46beb1e719b64
Thomas Waldmann <tw@waldmann-edv.de>
2014-09-29 11:31:22 +0200

    iptools: add docstring, minor changes

--- nsupdate/main/iptools.py
+++ nsupdate/main/iptools.py
@@ -4,14 +4,20 @@ Misc. IP tools: normalize, handle mapped addresses
 
 from netaddr import IPAddress
 
-def normalize_ip(ipaddr):
-    ipaddr = normalize_mapped_address(ipaddr)
-    return ipaddr
 
 def normalize_mapped_address(ipaddr):
-    ipaddr = IPAddress(ipaddr)
+    """
+    Converts a IPv4-mapped IPv6 address into a IPv4 address. Handles both the
+    ::ffff:192.0.2.128 format as well as the deprecated ::192.0.2.128 format.
 
+    :param ipaddr: IP address [str]
+    :return: normalized IP address [str]
+    """
+    ipaddr = IPAddress(ipaddr)
     if ipaddr.is_ipv4_compat() or ipaddr.is_ipv4_mapped():
         ipaddr = ipaddr.ipv4()
-
     return str(ipaddr)
+
+
+# currently, normalize_ip does no more than normalize_mapped_address:
+normalize_ip = normalize_mapped_address
