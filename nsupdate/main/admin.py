8974cf1dafbc31c3280d502ff0306f8af4bed7e5
Thomas Waldmann <tw@waldmann-edv.de>
2014-10-01 17:22:52 +0200

    fix pep8

--- nsupdate/main/admin.py
+++ nsupdate/main/admin.py
@@ -12,10 +12,12 @@ class DomainAdmin(admin.ModelAdmin):
     list_filter = ("created", "public", "available")
     search_fields = ("name", "created_by__username", "created_by__email")
 
+
 class HostAdmin(admin.ModelAdmin):
     list_display = ("name", "domain", "created_by", "client_faults", "abuse", "abuse_blocked")
     list_filter = ("created", "abuse", "abuse_blocked", "domain")
     search_fields = ("name", "created_by__username", "created_by__email")
+
 
 class BlacklistedHostAdmin(admin.ModelAdmin):
     list_display = ("name_re", "created_by")
