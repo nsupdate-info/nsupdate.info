c432e24c0dd61d0a62042672e9dd46469d85bf76
Thomas Waldmann <tw@waldmann-edv.de>
2014-11-16 23:05:35 +0100

    add a managment command "domains" to check the domains, notify owner of domain, update admin docs
    if a nameserver is not reachable or does not answer queries for the domain, the domain is flagged as not available (and also as not public).
    
    also added catching of PeerBadKey exception.

--- nsupdate/main/dnstools.py
+++ nsupdate/main/dnstools.py
@@ -318,6 +318,10 @@ def update_ns(fqdn, rdtype='A', ipaddr=None, action='upd', ttl=60):
         logger.error("PeerBadSignature - shared secret mismatch? zone: %s" % (origin, ))
         set_ns_availability(domain, False)
         raise DnsUpdateError("PeerBadSignature")
+    except dns.tsig.PeerBadKey:
+        logger.error("PeerBadKey - shared secret mismatch? zone: %s" % (origin, ))
+        set_ns_availability(domain, False)
+        raise DnsUpdateError("PeerBadKey")
     except dns.tsig.PeerBadTime:
         logger.error("PeerBadTime - DNS server did not like the time we sent. zone: %s" % (origin, ))
         set_ns_availability(domain, False)
