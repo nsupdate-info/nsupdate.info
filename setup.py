fb0967c0eccc01c095172e3f270bec1d4db1fa51
Thomas Waldmann <tw@waldmann-edv.de>
2018-09-30 23:04:28 +0200

    setup.py: don't require a specific Django version, so 1.1 and 2.x works
    also add py37 to pypi metadata.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     install_requires=[
         'dnspython',
         'netaddr',
-        'django~=1.11.0',
+        'django',
         'django-bootstrap-form',
         'django-registration-redux',
         'django-extensions',
@@ -68,6 +68,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: Name Service (DNS)',
     ],
 )
