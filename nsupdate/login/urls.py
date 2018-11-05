05d5a75c40253d3b6b01b9ea4ac3ce516e32dad3
Thomas Waldmann <tw@waldmann-edv.de>
2018-11-05 02:01:57 +0100

    fixing login.urls

--- nsupdate/login/urls.py
+++ nsupdate/login/urls.py
@@ -5,18 +5,21 @@ from django.contrib.auth.views import LoginView, LogoutView, \
 
 urlpatterns = (
     # login and logout url
-    url(r'^login/$', LoginView.as_view(), {'template_name': 'login.html'}, name='login'),
+    url(r'^login/$', LoginView.as_view(template_name='login.html'), name='login'),
     # or use logout with template 'logout.html'
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
 
     # password reset urls
-    url(r'^password_reset/$', PasswordResetView.as_view(), {'template_name': 'password_reset.html'},
+    url(r'^password_reset/$',
+        PasswordResetView.as_view(template_name='password_reset.html'),
         name='password_reset'),
-    url(r'^password_reset_done/$', PasswordResetDoneView.as_view(),
-        {'template_name': 'password_reset_done.html'}, name='password_reset_done'),
+    url(r'^password_reset_done/$',
+        PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+        name='password_reset_done'),
     url(r'^password_reset_confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
-        PasswordResetConfirmView.as_view(), {'template_name': 'password_reset_confirm.html'},
+        PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
         name='password_reset_confirm'),
-    url(r'^password_reset_complete/$', PasswordResetCompleteView.as_view(),
-        {'template_name': 'password_reset_complete.html'}, name='password_reset_complete'),
+    url(r'^password_reset_complete/$',
+        PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+        name='password_reset_complete'),
 )
