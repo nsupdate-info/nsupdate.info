9d8a50e92f8ac2afdc8f9126fe7222a91e6933ab
Florian Eitel <feitel@indeedgeek.de>
2023-01-14 13:39:30 +0100

    Update to Django 4.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     install_requires=[
         'dnspython',
         'netaddr',
-        'django>=4.0.0',
+        'django>=4.1.0',
         'django-bootstrap-form',
         'django-referrer-policy',
         'django-registration-redux',
