6a6d9a5ab2553e1615f1762b73593a6629f86ced
Florian Eitel <feitel@indeedgeek.de>
2023-01-14 13:05:22 +0100

    Update to Django 3.2

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     install_requires=[
         'dnspython',
         'netaddr',
-        'django>=3.1.0',
+        'django>=3.2.0',
         'django-bootstrap-form',
         'django-referrer-policy',
         'django-registration-redux',
