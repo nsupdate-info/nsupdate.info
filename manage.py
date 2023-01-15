5ea29a913b7dfb20745dc90229536b1d2c032b04
Florian Eitel <feitel@indeedgeek.de>
2023-01-15 12:47:06 +0100

    Add DEVELOPMENT.md to list common development commands
    Update `./manage.py` to find application folder in `src/` instead of
    expecting a toplevel `nsupdate` folder.

--- manage.py
+++ manage.py
@@ -4,6 +4,9 @@ import sys
 
 
 if __name__ == "__main__":
+    PROJECT_ROOT = os.path.dirname(__file__)
+    sys.path.insert(0, os.path.join(PROJECT_ROOT, 'src'))
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nsupdate.settings.dev")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
