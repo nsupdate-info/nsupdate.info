2cd520e71f874d0d37b6e02f11004b3659b50bf7
Thomas Waldmann <tw@waldmann-edv.de>
2023-03-18 21:48:02 +0100

    rename scripts/travis -> ci

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -7,15 +7,15 @@ sudo apt-get -y update
 sudo apt-get -y install bind9 dnsutils e2fsprogs
 
 sudo ln -s /var/lib/bind /etc/bind/zones
-sudo cp scripts/travis/etc/bind/named.conf.local /etc/bind/
+sudo cp scripts/ci/etc/bind/named.conf.local /etc/bind/
 sudo chown bind.bind /etc/bind/named.conf.local
-sudo cp scripts/travis/etc/bind/zones/* /etc/bind/zones/
+sudo cp scripts/ci/etc/bind/zones/* /etc/bind/zones/
 sudo chown bind.bind /etc/bind/zones/*
 sudo service bind9 restart
 
 sudo dpkg -P ubuntu-minimal resolvconf
 sudo rm -f /etc/resolv.conf
-sudo cp scripts/travis/etc/resolv.conf /etc/
+sudo cp scripts/ci/etc/resolv.conf /etc/
 sudo chattr +i /etc/resolv.conf
 
 dig @127.0.0.1 nsupdate.info SOA
@@ -23,5 +23,5 @@ dig @127.0.0.1 tests.nsupdate.info SOA
 sudo netstat -tulpen | grep 53
 
 pip install Django~=$DJANGO
-pip install -r requirements.d/travis.txt
+pip install -r requirements.d/ci.txt
 pip install -e .
