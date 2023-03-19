873afa586af3eae7641b8c294863904a8fc82fe2
Thomas Waldmann <tw@waldmann-edv.de>
2023-03-19 14:41:22 +0100

    fix DNS setup on gh action, fixes #509
    - enable TestReverseLookup again
    - add nslookup/host 1.1.1.1 debug cmds
    - output DNS / resolver configs before replacing them
    - bind9: use systemd-resolved as forwarder (guess we
      can not do full recursive lookups on gh actions)

--- scripts/ci/install-bind.sh
+++ scripts/ci/install-bind.sh
@@ -4,15 +4,31 @@
 sudo apt-get -y update
 
 # we install a local bind9 to run the tests against:
-sudo apt-get -y install bind9 dnsutils e2fsprogs
+sudo apt-get -y install bind9 bind9-dnsutils bind9-host e2fsprogs
 
-sudo ln -s /var/lib/bind /etc/bind/zones
+echo "named.conf.local"
+cat /etc/bind/named.conf.local
 sudo cp scripts/ci/etc/bind/named.conf.local /etc/bind/
 sudo chown bind.bind /etc/bind/named.conf.local
+
+echo "named.conf.options"
+cat /etc/bind/named.conf.options
+sudo cp scripts/ci/etc/bind/named.conf.options /etc/bind/
+sudo chown bind.bind /etc/bind/named.conf.options
+
+sudo ln -s /var/lib/bind /etc/bind/zones
 sudo cp scripts/ci/etc/bind/zones/* /etc/bind/zones/
 sudo chown bind.bind /etc/bind/zones/*
+
 sudo service bind9 restart
 
+echo "nsswitch.conf"
+ls -l /etc/nsswitch.conf
+cat /etc/nsswitch.conf
+
+echo "resolv.conf"
+ls -l /etc/resolv.conf
+cat /etc/resolv.conf
 sudo rm -f /etc/resolv.conf
 sudo cp scripts/ci/etc/resolv.conf /etc/
 sudo chattr +i /etc/resolv.conf
@@ -20,3 +36,6 @@ sudo chattr +i /etc/resolv.conf
 dig @127.0.0.1 nsupdate.info SOA
 dig @127.0.0.1 tests.nsupdate.info SOA
 sudo netstat -tulpen | grep 53
+
+nslookup 1.1.1.1
+host 1.1.1.1
