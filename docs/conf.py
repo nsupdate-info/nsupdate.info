6ee7ee4f490ebccae0a34df4d70cf758e6bd14e2
Peter Horvath <peter.horvath@pyur.com>
2026-04-06 02:06:48 +0200

    docs fixes: docs/_build gitignored, added sphinx extension to make latexpdf working again

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ import os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.viewcode', 'sphinx.ext.imgconverter']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
