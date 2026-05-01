3b8e2b1fc07b260de4939616a1fd7b778f36a6e8
Peter Horvath <horvath.akos.peter@gmail.com>
2026-05-01 17:46:15 +0200

    additional PR fixes/improvements based on review: cookie, script tags security params. nav-links are now always cursor:pointer. data-bs-theme is now greatly simplified

--- src/nsupdate/static/js/dark_mode_toggler.js
+++ src/nsupdate/static/js/dark_mode_toggler.js
@@ -1,13 +1,19 @@
 (function dark_mode_toggler(){
 
+  var theme_cookie_params = {
+    expires: 366,
+    path: '/',
+    sameSite: 'lax'
+  };
+
   function toggle_dark_mode() {
     var htmlNode = document.documentElement;
     if (htmlNode.getAttribute("data-bs-theme") === "dark") {
       htmlNode.setAttribute("data-bs-theme", "light");
-      Cookies.set('nsupdate_theme', 'light');
+      Cookies.set('nsupdate_theme', 'light', theme_cookie_params);
     } else {
       htmlNode.setAttribute("data-bs-theme", "dark");
-      Cookies.set('nsupdate_theme', 'dark');
+      Cookies.set('nsupdate_theme', 'dark', theme_cookie_params);
     }
   }
 
